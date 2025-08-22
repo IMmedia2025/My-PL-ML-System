e81d3a353901cd98f02e978a126c1e332ed7f86a
Stanley Anaduaka <139874422+stan-lii@users.noreply.github.com>
2025-08-22 14:45:10 +0100

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    serverComponentsExternalPackages: ['@tensorflow/tfjs-node', 'sqlite3']
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+        crypto: false,
+        stream: false,
+        url: false,
+        zlib: false,
+        http: false,
+        https: false,
+        assert: false,
+        os: false,
+        path: false,
+      };
+    }
+    
+    // Handle SQLite3 for server-side
+    if (isServer) {
+      config.externals.push('sqlite3');
+    }
+    
+    return config;
+  },
+}
+
+module.exports = nextConfig
